706e9bee2ec91c0c16dba9ef4f3799ec1072afec
User <elizaveta.terentieva1993@yandex.ru>
2023-03-29 20:54:56 +0300

    Tasks

--- homework5.py
+++ homework5.py
@@ -6,15 +6,28 @@
 A = 3; B = 5 -> 243 (3⁵)
     A = 2; B = 3 -> 8 
 '''
+'''
+def degree (n,m):
+    if m>0: return degree(n,m-1)*n
+    elif m<0: return degree(n, m+1)/n
+    else: return 1
 
-
+a=int(input())
+b=int(input())
+print(degree (a,b))
+'''
 '''
 Задача 28: Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных 
 чисел. Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
-
-
 *Пример:*
 
 2 2
     4
 '''
+def sum(a, b): 
+    if a==0: return b
+    else: return sum(a-1,b+1) 
+
+a=int(input())
+b=int(input())
+print(sum (a,b))
